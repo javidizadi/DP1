d0e87b644fd4f0a22d6a49aade92a18eb0ca3885
Javid Izadi <javid.izadi1384@gmail.com>
2024-11-13 11:48:25 +0330

    [refactor] error.cpp

--- error.cpp
+++ error.cpp
@@ -1,23 +1,3 @@
 #include "error.h"
-#include <iostream>
-using namespace std;
 
-void raiseStackError(StackError err) {
-
-  string msg;
-
-  switch (err) {
-
-  case StackError::StackIsFull:
-    msg = "Stack is full.";
-    break;
-
-  case StackError::StackIsEmpty:
-    msg = "Stack is empty.";
-    break;
-  }
-
-  cout << msg << endl;
-
-  throw err;
-}
+void raiseStackError(StackError err) { throw err; }

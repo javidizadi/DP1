95548e4adc2807d1bbb4444dca1005f18e986bf7
Javid Izadi <javid.izadi1384@gmail.com>
2024-11-15 20:39:21 +0330

    [add] fact function

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,16 @@
 #include "utils.h"
+#include <cstddef>
 #include <string>
 
 using namespace std;
 
 int validateInfix(string) { return -1; }
+
+size_t fact(size_t x) {
+
+  size_t fact = 1;
+  for (size_t i = 2; i < x; i++)
+    fact *= i;
+
+  return fact;
+}
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <cstddef>
 #include <string>
 
 // Should return -1 otherwise validation error occured
 int validateInfix(std::string);
+
+size_t fact(size_t);
